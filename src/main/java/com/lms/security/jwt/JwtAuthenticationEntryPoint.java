b240ea11547075f4b828530e9b08eaecf97c39ce
Vishal Narsinh <vishalnarsinh@gmail.com>
2025-08-02 22:29:29 +0530

    fixed some bugs

--- src/main/java/com/lms/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/lms/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package com.lms.security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lms.dto.ErrorResponseDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,22 +13,23 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        ErrorResponseDto errorResponse = new ErrorResponseDto(
-                request.getRequestURI(),
-                HttpStatus.UNAUTHORIZED,
-                HttpStatus.UNAUTHORIZED.value(),
-                getErrorMessage(authException),
-                LocalDateTime.now()
-        );
+        Map<String,Object> data = new LinkedHashMap<>();
+        data.put("apiPath",request.getRequestURI());
+        data.put("errorCodeString",HttpStatus.UNAUTHORIZED.value());
+        data.put("errorCode",HttpStatus.UNAUTHORIZED);
+        data.put("errorMessage",getErrorMessage(authException));
+        data.put("errorTime",LocalDateTime.now().toString());
 
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        new ObjectMapper().writeValue(response.getOutputStream(), errorResponse);
+        new ObjectMapper().writeValue(response.getOutputStream(),data);
     }
     private String getErrorMessage(AuthenticationException ex) {
         if (ex instanceof BadCredentialsException) return "Invalid credentials";
